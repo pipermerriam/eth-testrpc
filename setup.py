72d575e6113a25cd2b8b6aa24e2962385ead7aaa
Tim Coulter <tim@Tims-MacBook-Pro.local>
2015-08-07 12:02:23 -0700

    Maybe this will work?

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='eth-testrpc',
-    version='0.1.5',
+    version='0.1.6',
     description='An Ethereum simulator for aiding smart contract development.',
     long_description=long_description,
     url='https://github.com/ConsenSys/eth-testrpc',
-    download_url='https://github.com/Consensys/eth-testrpc/tarball/v0.1.5',
+    download_url='https://github.com/Consensys/eth-testrpc/archive/v0.1.6.tar.gz',
     author='ConsenSys',
     author_email='info@consensys.net',
     license='MIT',
