8ef05254ec22523dc51bc6ed68a168331deb1afa
Tim Coulter <tim@Tims-MacBook-Pro.local>
2015-10-24 12:39:02 -0700

    Trying this yet again, with no way to test it but by pushing to pypi...

--- testrpc/__init__.py
+++ testrpc/__init__.py
@@ -3,4 +3,8 @@ import os
 # This is gross, and ther ehas to be a better way,
 # but apparently the location of this file differs
 # when installed via pypi...
-VERSION = open(os.path.join(os.path.dirname(__file__), "../VERSION")).read().strip()
+file = os.path.join(os.path.dirname(__file__), "VERSION")
+if os.path.isfile(file) is False:
+    file = os.path.join(os.path.dirname(os.path.dirname(__file__)), "VERSION");
+
+VERSION = open(file).read().strip()
