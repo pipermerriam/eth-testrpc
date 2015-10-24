6b350042b6002f8ae3579cbfb7c29892aca0de67
Tim Coulter <tim@Tims-MacBook-Pro.local>
2015-10-24 13:05:49 -0700

    Finally something that'll probably work.

--- testrpc/__init__.py
+++ testrpc/__init__.py
@@ -1,2 +1,13 @@
-__version__ = "0.1.25"
-VERSION = __version__
+# Get the version from the file if the file exists.
+# If the file doesn't exist, that means we're running from pypi.
+
+import pkg_resources
+import os
+
+file = os.path.join(os.path.dirname(__file__), "../VERSION")
+if os.path.isfile(file) is True:
+    version = open(file).read().strip()
+else:
+    version = pkg_resources.get_distribution("eth-testrpc").version
+
+VERSION = version
