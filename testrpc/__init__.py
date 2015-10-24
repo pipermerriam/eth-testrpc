fa538f7a05dd9cff2c3baf175d45b179c406a898
Tim Coulter <tim@Tims-MacBook-Pro.local>
2015-10-24 12:47:11 -0700

    Perhaps seed still updates this value, and it was all my fault. I'm going mad, so I wouldn't be surprised.

--- testrpc/__init__.py
+++ testrpc/__init__.py
@@ -1,10 +1,2 @@
-import os
-
-# This is gross, and ther ehas to be a better way,
-# but apparently the location of this file differs
-# when installed via pypi...
-file = os.path.join(os.path.dirname(__file__), "VERSION")
-if os.path.isfile(file) is False:
-    file = os.path.join(os.path.dirname(os.path.dirname(__file__)), "VERSION");
-
-VERSION = open(file).read().strip()
+__version__ = "0.1.25"
+VERSION = __version__
