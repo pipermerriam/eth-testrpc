44758ecb2f0a6fc97539e10e2ff46bf9b0cb45ae
Piper Merriam <pipermerriam@gmail.com>
2016-06-14 16:32:36 -0600

    change how version is declared

--- testrpc/__init__.py
+++ testrpc/__init__.py
@@ -1,13 +1,4 @@
-# Get the version from the file if the file exists.
-# If the file doesn't exist, that means we're running from pypi.
-
 import pkg_resources
-import os
 
-file = os.path.join(os.path.dirname(__file__), "../VERSION")
-if os.path.isfile(file) is True:
-    version = open(file).read().strip()
-else:
-    version = pkg_resources.get_distribution("eth-testrpc").version
 
-VERSION = version
+__version__ = pkg_resources.get_distribution("eth-testrpc").version
