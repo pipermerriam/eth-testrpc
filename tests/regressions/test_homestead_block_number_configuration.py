be94e24c41c02fc5429aec37012e9aa4694078a2
Piper Merriam <pipermerriam@gmail.com>
2016-08-08 09:46:29 -0600

    fix flaky gas estimate test

---
+++ tests/regressions/test_homestead_block_number_configuration.py
@@ -0,0 +1,21 @@
+from eth_tester_client import EthTesterClient
+
+
+def test_homestead_block_defaults_to_0():
+    client = EthTesterClient()
+    assert client.evm.block.config['HOMESTEAD_FORK_BLKNUM'] == 0
+
+    client.mine_block()
+    client.mine_block()
+
+    assert client.evm.block.config['HOMESTEAD_FORK_BLKNUM'] == 0
+
+
+def test_dao_fork_block_defaults_to_0():
+    client = EthTesterClient()
+    assert client.evm.block.config['DAO_FORK_BLKNUM'] == 0
+
+    client.mine_block()
+    client.mine_block()
+
+    assert client.evm.block.config['DAO_FORK_BLKNUM'] == 0
