b4618e0ad077b2e392991d625ddf462bca1dc775
Piper Merriam <pipermerriam@gmail.com>
2016-10-04 14:34:04 -0600

    fix eth_newFilter for hex encoded block numbers

--- testrpc/utils.py
+++ testrpc/utils.py
@@ -34,3 +34,22 @@ def input_transaction_formatter(transaction):
         TXN_KWARGS_MAP.get(k, k): TXN_FORMATTERS.get(k, noop)(v)
         for k, v in transaction.items()
     }
+
+
+FILTER_KWARGS_MAP = {
+    'fromBlock': 'from_block',
+    'toBlock': 'to_block',
+}
+
+
+FILTER_FORMATTERS = {
+    'fromBlock': normalize_block_number,
+    'toBlock': normalize_block_number,
+}
+
+
+def input_filter_params_formatter(filter_params):
+    return {
+        FILTER_KWARGS_MAP.get(k, k): FILTER_FORMATTERS.get(k, noop)(v)
+        for k, v in filter_params.items()
+    }
