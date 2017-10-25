a19bd2b14e0b8fbeba25364d63c6f7578cc8c152
Piper Merriam <pipermerriam@gmail.com>
2017-10-25 11:40:19 -0600

    1.3.1 Release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='eth-testrpc',
-    version="1.3.0",
+    version="1.3.1",
     description='An Ethereum simulator for aiding smart contract development.',
     long_description=long_description,
     url='https://github.com/pipermerriam/eth-testrpc',
